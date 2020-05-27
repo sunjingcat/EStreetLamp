4a331c4d98852c72d2a9dd6aff731a0e109b8c9a
sunjing <sj2012518@126.com>
2020-05-27 14:01:56 +0800

    bug

---
+++ app/src/main/java/com/zz/lamp/Main2Activity.kt
@@ -0,0 +1,17 @@
+package com.zz.lamp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main2.*
+
+class Main2Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+
+        bt.setOnClickListener {
+            tv.setText("变化的Tv")
+        }
+    }
+}
