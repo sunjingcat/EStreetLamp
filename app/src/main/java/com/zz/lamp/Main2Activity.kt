772fbdcece33f99f51dc84d1f6d55d5d0527a23c
sunjing <sj2012518@126.com>
2020-06-10 14:06:46 +0800

    bug

--- app/src/main/java/com/zz/lamp/Main2Activity.kt
+++ app/src/main/java/com/zz/lamp/Main2Activity.kt
@@ -6,7 +6,6 @@ import androidx.appcompat.app.AppCompatActivity
 import com.zz.lamp.emen.Device
 import com.zz.lamp.emen.TestProvider
 import kotlinx.android.synthetic.main.activity_main2.*
-import kotlin.reflect.KClass
 
 @TestProvider(id = 1, type = "message", json = Device::class)
 class Main2Activity : AppCompatActivity() {
@@ -17,13 +16,9 @@ class Main2Activity : AppCompatActivity() {
 
         bt.setOnClickListener {
             tv.setText("变化的Tv")
-            try {
-
-                val cls = Class.forName("com.zz.lamp.emen.TestProvider")
-                val testProvider= TestProvider::class
-                testProvider::id
-            } catch (e: Exception) {
-            }
+            val clazz = Main2Activity::class.java
+            val annotation = clazz.getAnnotation(TestProvider::class.java)
+            println("类注解的值：${annotation?.id},${annotation?.type}")
         }
 
     }
